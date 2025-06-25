08dbd87dd9eff02e190f98ddf8dc77f3125aca86
nimaofficial <aminnnzein@gmail.com>
2025-06-26 03:42:05 +0700

    Add files via upload

---
+++ game.js
@@ -0,0 +1,9 @@
+
+const canvas = document.getElementById('game');
+const ctx = canvas.getContext('2d');
+
+ctx.fillStyle = '#45a29e';
+ctx.fillRect(50, 120, 300, 60);
+ctx.fillStyle = '#fff';
+ctx.font = '18px monospace';
+ctx.fillText('Nemisurai Combat Simulation', 70, 155);
